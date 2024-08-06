fc65ec06b388d60ee4e2a60fa48128c36ae0ed60
Sachin K.C. <sachinxpml6@gmail.com>
2024-08-06 15:52:56 -0700

    vanillaTStodoCompleted

--- VanillaTS/src/main.ts
+++ VanillaTS/src/main.ts
@@ -1,24 +1,66 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface Todo {
+  text: string;
+  isCompleted: boolean;
+  readonly id: string;
+}
+let todos: Todo[] = [];
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const todoContainer = document.querySelector(
+  ".todoContainer"
+) as HTMLDivElement;
+const todoForm = document.querySelector(".todoForm") as HTMLFormElement;
+const todoInput = document.querySelector(".todoInput") as HTMLInputElement;
+
+todoForm.onsubmit = (e) => {
+  e.preventDefault();
+  const todo: Todo = {
+    text: todoInput.value,
+    isCompleted: false,
+    id: String(Math.random() * 1000),
+  };
+  todos.push(todo);
+  todoInput.value = "";
+  renderTodos(todos);
+};
+
+const renderTodos = (todos: Todo[]) => {
+  todoContainer.innerHTML = ``;
+  todos.forEach((item) => {
+    generateTodo(item);
+  });
+};
+
+const generateTodo = (item: Todo) => {
+  const todoBox = document.createElement("div") as HTMLDivElement;
+  todoBox.className = "todoBox  ";
+
+  //delte button
+  const deletebtn = document.createElement("button") as HTMLButtonElement;
+  deletebtn.innerText = `X`;
+  deletebtn.addEventListener("click", () => deleteAction(item.id));
+
+  //checkbox
+  const checkbox = document.createElement("input") as HTMLInputElement;
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = item.isCompleted;
+  checkbox.onchange = () => {
+    item.isCompleted = checkbox.checked;
+    para.className = item.isCompleted ? "underline" : "";
+  };
+  //todo title
+  const para = document.createElement("p") as HTMLParagraphElement;
+  para.innerText = item.text;
+  para.className = item.isCompleted ? "underline" : "";
+
+  todoBox.append(checkbox, para, deletebtn);
+  todoContainer.append(todoBox);
+};
+
+const deleteAction = (id: string) => {
+  todos = todos.filter((todo) => {
+    return todo.id != id;
+  });
+  renderTodos(todos);
+};
